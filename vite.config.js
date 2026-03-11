b343c8fe261c075aa9c365f49c88ce40ab12ba2c
k21447841-maker <k21447841@gmail.com>
2026-03-11 14:08:50 +0530

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
+  server: { port: 3000, host: true },
+  build: { outDir: 'dist', sourcemap: false }
+})
